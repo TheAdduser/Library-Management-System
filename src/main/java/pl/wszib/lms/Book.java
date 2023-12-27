a65f495aaa6af98c73a522a897af04ddee663a29
Bartosz Wrona <EasyRider190@gmail.com>
2023-12-27 12:43:10 +0100

    feat/add id to book

--- src/main/java/pl/wszib/lms/Book.java
+++ src/main/java/pl/wszib/lms/Book.java
@@ -1,12 +1,15 @@
 package pl.wszib.lms;
 
+
 public class Book {
-    String isbn;
-    String title;
-    String author;
+    private int id;
+    private String isbn;
+    private String title;
+    private String author;
 
 
     public Book(String isbn) {
+        this.id = id;
         this.isbn = isbn;
         this.title = title;
         this. author = author;
