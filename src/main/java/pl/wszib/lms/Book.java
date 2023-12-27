311e9378fbdeb05fb16d13ce0dc861d6b0edcc96
Bartosz Wrona <EasyRider190@gmail.com>
2023-12-27 12:51:19 +0100

    add dates to book

--- src/main/java/pl/wszib/lms/Book.java
+++ src/main/java/pl/wszib/lms/Book.java
@@ -1,17 +1,23 @@
 package pl.wszib.lms;
 import java.util.*;
+import java.time.LocalDate;
 
 public class Book {
     private int id;
     private String isbn;
     private String title;
     private String author;
+    private LocalDate leaseStartDate;
+    private LocalDate leaseEndDate;
+
 
 
     public Book(int id) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
-        this. author = author;
+        this.author = author;
+        this.leaseStartDate;
+        this.leaseEndDate = leaseStartDate.plusWeeks(2);
     }
 }
