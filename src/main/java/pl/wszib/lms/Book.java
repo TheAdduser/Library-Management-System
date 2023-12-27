4f2cf35ecf71234b217bd5a9a9b6cd04f884b682
Bartosz Wrona <EasyRider190@gmail.com>
2023-12-27 11:47:37 +0100

    add book constructor

---
+++ src/main/java/pl/wszib/lms/Book.java
@@ -0,0 +1,14 @@
+package pl.wszib.lms;
+
+public class Book {
+    String isbn;
+    String title;
+    String author;
+
+
+    public Book(String isbn) {
+        this.isbn = isbn;
+        this.title = title;
+        this. author = author;
+    }
+}
