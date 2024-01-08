5ca1bbf488b9c2288af351232ef2a14d6781781f
Bartosz Wrona <EasyRider190@gmail.com>
2024-01-08 13:11:09 +0100

    removed redundant id

--- src/main/java/pl/wszib/lms/model/Book.java
+++ src/main/java/pl/wszib/lms/model/Book.java
@@ -1,22 +1,18 @@
 package pl.wszib.lms.model;
-import java.util.*;
 import java.time.LocalDate;
 
 public class Book {
-    
-    private final String bookId;
+
     private String isbn;
     private String title;
     private String author;
     private LocalDate leaseStartDate;
     private LocalDate leaseEndDate;
-    private int userID;
 
 
 
 public Book(String isbn, String title, String author) {
 
-    this.bookId = UUID.randomUUID().toString();
     this.isbn = isbn;
     this.title = title;
     this.author = author;
@@ -43,12 +39,9 @@ public Book(String isbn, String title, String author) {
         this.author = author;
     }
 
-    public String getBookId() {return bookId; }
     public LocalDate getLeaseStartDate() {return leaseStartDate; }
     public void SetLeaseStartDate(LocalDate leaseStartDate) {this.leaseStartDate = leaseStartDate; }
     public LocalDate getLeaseEndDate() {return leaseEndDate; }
     public void SetLeaseEndDate(LocalDate leaseEndDate) {this.leaseEndDate = leaseStartDate.plusWeeks(2); }
 
-    public int getUserID() {return userID; }
-    public void setUserID(int userID) {this.userID = userID; }
 }
