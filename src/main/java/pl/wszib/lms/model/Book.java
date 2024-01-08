7ee61885c7e59a6bdab0a2d3be53ef753fe50c5b
Bartosz Wrona <EasyRider190@gmail.com>
2024-01-08 11:47:55 +0100

    update book model

--- src/main/java/pl/wszib/lms/model/Book.java
+++ src/main/java/pl/wszib/lms/model/Book.java
@@ -4,23 +4,26 @@ import java.time.LocalDate;
 
 public class Book {
     
-    private String id;
+    private String bookId;
     private String isbn;
     private String title;
     private String author;
     private LocalDate leaseStartDate;
     private LocalDate leaseEndDate;
+    private int userID;
 
 
 
 public Book(String isbn, String title, String author, LocalDate leaseStartDate) {
 
-    this.id = UUID.randomUUID().toString();
+    this.bookId = UUID.randomUUID().toString();
     this.isbn = isbn;
     this.title = title;
     this.author = author;
-    this.leaseStartDate;
+    this.leaseStartDate = leaseStartDate;
     this.leaseEndDate = leaseStartDate.plusWeeks(2);
+    this.userID = userID;
+
 }
 
 }
