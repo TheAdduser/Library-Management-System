0ef3fab29b1d5ea07b1e760d06c5f5a9e79c9346
Bartosz Wrona <EasyRider190@gmail.com>
2024-01-08 12:01:59 +0100

    Add first gui menu

---
+++ src/main/java/pl/wszib/lms/gui/GUI.java
@@ -0,0 +1,18 @@
+package pl.wszib.lms.gui;
+
+import pl.wszib.lms.authorization.Authenticator;
+import pl.wszib.lms.model.Book;
+import pl.wszib.lms.model.User;
+
+import java.util.Collection;
+import java.util.Scanner;
+public class GUI {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static String showMenuAndReadChoose() {
+        System.out.println("1. List books");
+        System.out.println("2. Rent book");
+        System.out.println("3. Return book");
+        System.out.println("4. Exit");
+        return scanner.nextLine();
+    }
+}
