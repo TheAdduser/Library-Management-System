ec525d6077f8b304903ff9475f05e069b6127125
TheAdduser <EasyRider190@gmail.com>
2024-01-13 13:23:19 +0100

    fix BookRepository

--- src/main/java/pl/wszib/lms/db/BookRepository.java
+++ src/main/java/pl/wszib/lms/db/BookRepository.java
@@ -11,16 +11,16 @@ public class BookRepository {
     private final ArrayList<Book> books = new ArrayList<>();
 
     public BookRepository() {
-        this.books[0] = new Book("9788304004467", "Pan Tadeusz", "Adam Mickiewicz");
-        this.books[1] = new Book("9782824903996", "Krzyżacy", "Henryk Sienkiewicz");
-        this.books[2] = new Book("9788373271890", "W pustyni i w puszczy", "Henryk Sienkiewicz");
-        this.books[3] = new Book("9788380080666", "Chłopi", "Władysław Reymont");
-        this.books[4] = new Book("9788373191724", "Lalka", "Bolesław Prus");
-        this.books[5] = new Book("9788389681111", "Ferdydurke", "Witold Gombrowicz");
-        this.books[6] = new Book("9788389598570", "Zbrodnia i kara", "Fiodor Dostojewski");
-        this.books[7] = new Book("9788386802546", "Quo Vadis", "Henryk Sienkiewicz");
-        this.books[8] = new Book("9788324014251", "Nad Niemnem", "Eliza Orzeszkowa");
-        this.books[9] = new Book("9788380108574", "Solaris", "Stanisław Lem");
+        this.books.add(new Book("9788304004467", "Pan Tadeusz", "Adam Mickiewicz"));
+        this.books.add(new Book("9782824903996", "Krzyżacy", "Henryk Sienkiewicz"));
+        this.books.add(new Book("9788373271890", "W pustyni i w puszczy", "Henryk Sienkiewicz"));
+        this.books.add(new Book("9788380080666", "Chłopi", "Władysław Reymont"));
+        this.books.add(new Book("9788373191724", "Lalka", "Bolesław Prus"));
+        this.books.add(new Book("9788389681111", "Ferdydurke", "Witold Gombrowicz"));
+        this.books.add(new Book("9788389598570", "Zbrodnia i kara", "Fiodor Dostojewski"));
+        this.books.add(new Book("9788386802546", "Quo Vadis", "Henryk Sienkiewicz"));
+        this.books.add(new Book("9788324014251", "Nad Niemnem", "Eliza Orzeszkowa"));
+        this.books.add(new Book("9788380108574", "Solaris", "Stanisław Lem"));
     }
 
     public ArrayList<Book> getBooks() {
